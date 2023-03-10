11082a7ebde55a79d30859199934df0bbb0e004c
marvicss <marconivictor15@gmail.com>
2023-03-09 23:02:41 -0300

    ajuste

---
+++ aula2/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome:"joão",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550","1115555444"] 
+};
+
+console.log(cliente.telefone);
+
+cliente.endereço = {
+    rua:"Rua da Aurora",
+    numero: 1888,
+    apartamento : true,
+    complemento: "ap 934"
+}
